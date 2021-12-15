bed9688e116a5cc3c7f7bfdceef3227d38c5fcc9
XiangPan <xiangpan@nyu.edu>
2021-12-14 19:47:27 -0500

    project

--- project/scripts/low_kitti.sh
+++ project/scripts/low_kitti.sh
@@ -2,10 +2,10 @@
 ###
  # @Author: Xiang Pan
  # @Date: 2021-12-14 18:46:35
- # @LastEditTime: 2021-12-14 18:47:35
+ # @LastEditTime: 2021-12-14 19:36:00
  # @LastEditors: Xiang Pan
  # @Description: 
  # @FilePath: /project/scripts/low_kitti.sh
  # @email: xiangpan@nyu.edu
 ### 
-wan
+python main.py --gpu 0 --task_name=KITTI --load_checkpoint_path ./cached_models/unity-streetview-low-res_mIOU=0.77.ckpt --log_name=low_kitti
