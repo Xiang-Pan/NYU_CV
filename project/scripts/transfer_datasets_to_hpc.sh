7912827ce0547f716818a5dc3f99196bf0fc33cf
XiangPan <xiangpan@nyu.edu>
2021-12-16 23:36:03 -0500

    update

--- project/scripts/transfer_datasets_to_hpc.sh
+++
@@ -1,9 +0,0 @@
-###
- # @Author: Xiang Pan
- # @Date: 2021-12-14 19:36:34
- # @LastEditTime: 2021-12-14 19:36:35
- # @LastEditors: Xiang Pan
- # @Description: 
- # @FilePath: /project/scripts/transfer_datasets_to_hpc.sh
- # @email: xiangpan@nyu.edu
-### 
